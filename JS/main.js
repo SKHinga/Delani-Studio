25261d3b23a63d01890be53e151f38a6d77349f6
SKHinga <samuelhinga200@gmail.com>
2022-03-13 21:57:49 +0300

    Script: Utilize arrays to perform hover and click events.

--- JS/main.js
+++ JS/main.js
@@ -1,55 +1,23 @@
-$('.toggle').click(()=>{
-  $('.paragraph').toggle();
-  $('.icon').toggle();
-})
-$('.tog').click(()=>{
-  $('.graph').toggle();
-  $('.con').toggle();
-})
-$('.togg').click(()=>{
-  $('.para').toggle();
-  $('.ico').toggle();
-})
-$('#hover').hover(()=>{
-  $('#overlay').css('opacity', '1')
-}, function(){
-  $('#overlay').css('opacity', '0')
-})
-$('#cursor').hover(()=>{
-  $('#lay').css('opacity', '1')
-}, function(){
-  $('#lay').css('opacity', '0')
-})
-$('#pose').hover(()=>{
-  $('#over').css('opacity', '1')
-}, function(){
-  $('#over').css('opacity', '0')
-})
-$('#col').hover(()=>{
-  $('#mount').css('opacity', '1')
-}, function(){
-  $('#mount').css('opacity', '0')
-})
-$('#first').hover(()=>{
-  $('#call').css('opacity', '1')
-}, function(){
-  $('#call').css('opacity', '0')
-})
-$('#second').hover(()=>{
-  $('#invite').css('opacity', '1')
-}, function(){
-  $('#invite').css('opacity', '0')
-})
-$('#third').hover(()=>{
-  $('#arrive').css('opacity', '1')
-}, function(){
-  $('#arrive').css('opacity', '0')
-})
-$('#fourth').hover(()=>{
-  $('#depart').css('opacity', '1')
-}, function(){
-  $('#depart').css('opacity', '0')
-})
+let columns = ['toggle', 'tog', 'togg'];
+columns.forEach((col)=>{
+  $(`#${col}-col`).click((e)=>{
+    e.preventDefault();
+    $(`.${col}-info`).toggle();
+    $(`.${col}-icon`).toggle();
+  })
+});
+
+
+let projects = ['hover', 'cursor', 'pose', 'col', 'first', 'second', 'third', 'fourth']
+projects.forEach((project)=>{
+  $(`#${project}`).hover(()=>{
+    $(`#${project} .overlay`).css('opacity',1);
+  }, ()=>{
+    $(`#${project} .overlay`).css('opacity',0);
+  })
+});
+
+
 $('button:submit').click(()=>{
   alert("Thank you for contacting us! We'll be sure to get back to you.");
-})
+});
