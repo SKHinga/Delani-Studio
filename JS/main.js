ee6aa13a1f57957d9ee7e2074a1c3dd7b70f13db
SKHinga <samuelhinga200@gmail.com>
2022-03-13 12:02:59 +0300

    Ft-Script: Add ids and query hover mouse even.

--- JS/main.js
+++ JS/main.js
@@ -10,3 +10,43 @@ $('.togg').click(()=>{
   $('.para').toggle();
   $('.ico').toggle();
 })
+$('#hover').hover(()=>{
+  $('#overlay').css('opacity', '1')
+}, function(){
+  $('#overlay').css('opacity', '0')
+})
+$('#cursor').hover(()=>{
+  $('#lay').css('opacity', '1')
+}, function(){
+  $('#lay').css('opacity', '0')
+})
+$('#pose').hover(()=>{
+  $('#over').css('opacity', '1')
+}, function(){
+  $('#over').css('opacity', '0')
+})
+$('#col').hover(()=>{
+  $('#mount').css('opacity', '1')
+}, function(){
+  $('#mount').css('opacity', '0')
+})
+$('#first').hover(()=>{
+  $('#call').css('opacity', '1')
+}, function(){
+  $('#call').css('opacity', '0')
+})
+$('#second').hover(()=>{
+  $('#invite').css('opacity', '1')
+}, function(){
+  $('#invite').css('opacity', '0')
+})
+$('#third').hover(()=>{
+  $('#arrive').css('opacity', '1')
+}, function(){
+  $('#arrive').css('opacity', '0')
+})
+$('#fourth').hover(()=>{
+  $('#depart').css('opacity', '1')
+}, function(){
+  $('#depart').css('opacity', '0')
+})
